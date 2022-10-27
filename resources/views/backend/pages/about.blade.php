2a07cc2a3cebd787c80ecc1448240bdbcc895544
MolyFA <farjanaaktermoly17@gmail.com>
2022-10-27 10:17:17 +0600

    Adding Employee table

--- resources/views/backend/pages/about.blade.php
+++ resources/views/backend/pages/about.blade.php
@@ -1,4 +1,39 @@
 @extends('backend.master')
 @section('content')
-<h1>Employee Payroll System</h1>
+<h1>Employee Details</h1>
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Address</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>X</td>
+      <td>Uttara</td>
+      
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Y</td>
+      <td>Banani</td>
+     
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Z</td>
+      <td>Mirpur</td>
+      
+    </tr>
+  </tbody>
+</table>
+
+
+
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use App\Http\Controllers\HomeController;
 
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,6 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',[HomeController::class,'home']);
 Route::get('/about',[HomeController::class,'aboutus']);
 Route::get('/admin',[HomeController::class,'dashboard']);
+
+
 
 
 
