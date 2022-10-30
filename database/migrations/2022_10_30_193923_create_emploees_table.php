1b8dfcc496ca4d6973dd21deefc1e2d4df1fce7f
MolyFA <farjanaaktermoly17@gmail.com>
2022-10-31 02:17:15 +0600

    migration

---
+++ database/migrations/2022_10_30_193923_create_emploees_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('emploees', function (Blueprint $table) {
+            $table->id();
+            $table->string(column:'name',length: 50);
+            $table->string(column:'phone_number',length: 20);
+            $table->string(column:'email',length:25);
+            $table->text(column:'address',);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('emploees');
+    }
+};
